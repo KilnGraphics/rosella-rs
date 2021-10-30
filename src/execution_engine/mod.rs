058c90416701dda87629ea809fc491fbaed471ac
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2021-10-30 15:02:12 +0200

    Merged master

--- src/execution_engine/mod.rs
+++ src/execution_engine/mod.rs
@@ -1,4 +1,3 @@
-
 mod resource_state;
 mod static_resource_state;
 mod object_manager;
--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,6 @@ pub mod rosella;
 pub mod shader;
 pub mod util;
 pub mod window;
+pub mod execution_engine;
 
 pub use util::allocation_callbacks::ALLOCATION_CALLBACKS;
